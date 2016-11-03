41ca07a3174a20ce3b2c75cfcffd20faa13e9a68
Jess <jess.harrelson0707@gmail.com>
2016-11-03 18:12:44 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function iterativeLog(array){
+  array.forEach((element, index) => {
+    console.log(`${index}: ${element}`)
+  })
+}
+
+function iterate(callback) {
+  var array = [1, 2, 3]
+  array.forEach(callback)
+  return array
+}
+
+function doToArray(array, callback){
+  array.forEach(callback)
+}
